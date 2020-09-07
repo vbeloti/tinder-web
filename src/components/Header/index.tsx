85ab034165426c56fa7377248f784d0af9a6013d
unknown <viniciusbeloti7@gmail.com>
2020-09-07 10:32:27 -0300

    feat: add Header Styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
+import PersonIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
+import IconButton from "@material-ui/core/IconButton";
 
-import './styles.css';
+import logo from "../../assets/img/logo.svg";
+import "./styles.css";
 
 const Header: React.FC = () => {
   return (
-      <div className="header">
-          Header
-      </div>
+    <div className="header">
+      <IconButton>
+        <PersonIcon className="header__icon" fontSize="large" />
+      </IconButton>
+
+      <IconButton>
+        <img className="header__logo" src={logo} alt="Tinder logo" />
+      </IconButton>
+
+      <IconButton>
+        <ForumIcon className="header__icon" fontSize="large" />
+      </IconButton>
+    </div>
   );
-}
+};
 
 export default Header;
