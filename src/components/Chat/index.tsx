58999c6ab451a43ed8e761a23d5c48d82ecbe607
unknown <viniciusbeloti7@gmail.com>
2020-09-07 12:34:43 -0300

    feat: add Chat styles

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./styles.css";
+import { Avatar } from "@material-ui/core";
 
 interface IChat {
   name: string;
@@ -10,7 +11,16 @@ interface IChat {
 }
 
 const Chat: React.FC<IChat> = ({ name, message, timestamp, profilePic }) => {
-  return <div className="chat"></div>;
+  return (
+    <div className="chat">
+      <Avatar className="chat__image" alt={name} src={profilePic} />
+      <div className="chat__details">
+        <h2>{name}</h2>
+        <p>{message}</p>
+      </div>
+      <p className="chat__timestamp">{timestamp}</p>
+    </div>
+  );
 };
 
 export default Chat;
--- src/components/Chats/index.tsx
+++ src/components/Chats/index.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 import "./styles.css";
+import Chat from "../Chat";
 
 const Chats: React.FC = () => {
   return (
     <div className="chats">
-      <h1>Chats</h1>
+      <Chat
+        name="Carol"
+        message="Oi!"
+        timestamp="50 segundos atrás"
+        profilePic="https://terrigen-cdn-dev.marvel.com/content/prod/1x/008cmv_ons_crd_04.jpg"
+      />
+
+      <Chat
+        name="Natasha"
+        message="Olá!"
+        timestamp="10 segundos atrás"
+        profilePic="https://terrigen-cdn-dev.marvel.com/content/prod/1x/011blw_ons_crd_03.jpg"
+      />
+
+      <Chat
+        name="Thor"
+        message="Olá!"
+        timestamp="50 segundos atrás"
+        profilePic="https://terrigen-cdn-dev.marvel.com/content/prod/1x/004tho_ons_crd_03.jpg"
+      />
     </div>
   );
 };
