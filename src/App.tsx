93049a877e90ca378edf99bb83d69ff27fd3fb08
unknown <viniciusbeloti7@gmail.com>
2020-09-07 10:40:47 -0300

    feat: add react tinder cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>Chat</h1>
+          </Route>
+          <Route path="/">
+           Home
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/TinderCards/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const TinderCards: React.FC = () => {
+  return (
+    <div className="tinderCards">
+
+    </div>
+  );
+}
+
+export default TinderCards;
